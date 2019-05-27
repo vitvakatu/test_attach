211f3d27980ec9c249caa2e56ff604b378d050f1
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-05-27 22:25:32 +0300

    Experiments

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,25 @@ use std::sync::Arc;
 use std::thread::{spawn, sleep};
 use std::mem::forget;
 use std::time::Duration;
-use crate::executors::{HackyExecutor, JniExecutor};
-
-mod executors;
 
 fn main() {
-    let jvm = jni::JavaVM::new(InitArgsBuilder::new().version(JNIVersion::V8).build().unwrap()).unwrap();
+    let init_args = InitArgsBuilder::new().version(JNIVersion::V8)
+        .option("-XX:+HeapDumpOnOutOfMemoryError")
+        .option("-Xmx4m").build().unwrap();
+    let jvm = jni::JavaVM::new(init_args).unwrap();
     let jvm = Arc::new(jvm);
 
-    let executor = HackyExecutor::new(jvm, 2);
-
-    let executor1 = executor.clone();
-
-    let thread1 = spawn(move || {
-        executor1.with_attached(|env| {
-            println!("Some operation");
-            sleep(Duration::from_millis(1000));
-            println!("Finished");
-            Ok(())
-        }).unwrap();
-    });
-
-    let executor2 = executor.clone();
-
-    let thread2 = spawn(move || {
-        executor2.with_attached(|env| {
-            println!("Another operation");
-            sleep(Duration::from_millis(500));
-            println!("Finished 2");
-            Ok(())
-        }).unwrap();
-    });
-
-    sleep(Duration::from_millis(2000));
+    let mut counter = 0;
+    loop {
+        let jvm_clone = jvm.clone();
+        let thread = spawn(move || {
+            let env = jvm_clone.attach_current_thread().unwrap();
+            forget(env);
+        });
+        sleep(Duration::from_millis(1));
+        counter += 1;
+        if counter % 100 == 0 {
+            println!("{}", counter);
+        }
+    }
 }
